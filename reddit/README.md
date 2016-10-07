9bbe9d86ee2ba462353e710bed24aee4dd2bb20e
Mahmoud <md2989@nyu.edu>
2016-10-07 02:16:28 -0400

    Added reddit wordcloud

---
+++ reddit/README.md
@@ -0,0 +1,2 @@
+# reddit-wordcloud
+Takes all of the comments of a Reddit account and makes a word cloud. 
---
+++ reddit/reddit_wordcloud.py
@@ -0,0 +1,31 @@
+#! usr/bin/env python3
+
+import matplotlib.pyplot as pylt
+import praw
+from wordcloud import WordCloud
+
+def get_user(r):
+    username = input("Enter Reddit username: ")
+    user = r.get_redditor(username)
+    return user
+
+def get_comments(user):
+    comments = '' 
+    for c in user.get_comments(limit=None):
+        comments = comments + ' ' + c.body
+    return comments
+    
+def make_wordcloud(comments):
+    cloud = WordCloud().generate(comments)
+    pylt.imshow(cloud)
+    pylt.axis("off")
+    pylt.show()
+    
+def main():
+    r = praw.Reddit(user_agent="Get User and make wordcloud of comments")
+    user = get_user(r)
+    comments = get_comments(user)
+    make_wordcloud(comments)
+    
+if __name__ == "__main__":
+    main()
