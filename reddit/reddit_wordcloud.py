7ddc979a414c871e5bea4bea7f2384aa032e96bc
Mahmoud <md2989@nyu.edu>
2017-01-12 19:53:27 -0500

    Added function docstrings

--- reddit/reddit_wordcloud.py
+++ reddit/reddit_wordcloud.py
@@ -1,21 +1,25 @@
 #! /usr/bin/env python3
+"""Gets all of the comments written by a reddit user and outputs a word cloud of them"""
 
 import matplotlib.pyplot as pylt
 import praw
 from wordcloud import WordCloud
 
 def get_user(r):
+    """Returns a user object"""
     username = input("Enter Reddit username: ")
     user = r.get_redditor(username)
     return user
 
 def get_comments(user):
-    comments = '' 
-    for c in user.get_comments(limit=None):
-        comments = comments + ' ' + c.body
+    """Returns all of the user's comments"""
+    comments = ''
+    for comment in user.get_comments(limit=None):
+        comments = comments + ' ' + comment.body
     return comments
     
 def make_wordcloud(comments):
+    """Creates a word cloud based on the string input"""
     cloud = WordCloud().generate(comments)
     pylt.imshow(cloud)
     pylt.axis("off")
@@ -26,3 +30,4 @@ if __name__ == "__main__":
     user = get_user(r)
     comments = get_comments(user)
     make_wordcloud(comments)
+    
